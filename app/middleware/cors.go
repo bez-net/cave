7ceaf0a1948c18b14e7ea4c00cb7e9652154ead4
Onojeta Brown <onosbrown.saved@gmail.com>
2021-09-25 13:05:00 +0100

    first commit

---
+++ app/middleware/cors.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/rs/cors"
+)
+
+func Cors(handler http.Handler) http.Handler {
+	handleCORS := cors.Default().Handler
+	return handleCORS(handler)
+}
+
+// func Middleware(h http.Handler) http.Handler {
+//     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//         log.Println("middleware", r.URL)
+//         h.ServeHTTP(w, r)
+//     })
+// }
